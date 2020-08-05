05a71de647e903888012aee27d3d402c2c820983
shybyte <shybyte@users.noreply.github.com>
2020-08-05 07:27:36 +0200

    Started with wahrheit.ts

---
+++ src/effects/control-sequence-stepper.ts
@@ -0,0 +1,38 @@
+import {OSC2_SEMITONE} from '../microkorg';
+import {MidiEvent} from '../midi-event';
+import {MidiFilter} from '../midi-filter';
+import {MidiMessageRaw} from '../midi-message';
+import {MidiOut} from '../midi-out';
+import {Effect} from '../patch';
+
+export interface ControlSequenceStepperProps {
+  trigger: MidiFilter;
+  resetFilter: MidiFilter;
+  outputPortName: string;
+  control: number,
+  values: number[];
+}
+
+export class ControlSequenceStepper implements Effect {
+  private valueIndex = 0;
+
+  constructor(private props: ControlSequenceStepperProps) {
+  }
+
+  async onMidiEvent(midiEvent: MidiEvent, midiOut: MidiOut) {
+    const props = this.props;
+
+    if (!(props.trigger(midiEvent))) {
+      return;
+    }
+
+    if (props.resetFilter(midiEvent)) {
+      this.valueIndex = 0;
+    }
+
+    const value = props.values[this.valueIndex];
+    console.log('Send Value!', value);
+    this.valueIndex = (this.valueIndex + 1) % props.values.length;
+    midiOut.send(props.outputPortName, MidiMessageRaw.controlChange(props.control, value));
+  }
+}
---
+++ src/songs/wahrheit.ts
@@ -0,0 +1,46 @@
+import {ControlSequenceStepper} from '../effects/control-sequence-stepper';
+import {HarmonyDrum} from '../effects/harmony-drum';
+import {CUTOFF, OSC2_SEMITONE} from '../microkorg';
+import {MidiEvent} from '../midi-event';
+import {filterBy, filterByPort} from '../midi-filter';
+import {MidiMessageRaw} from '../midi-message';
+import {MidiOut} from '../midi-out';
+import {Patch} from '../patch';
+import {mapRange} from '../utils';
+import {HAND_SONIC, MidiThroughPort, USB_MIDI_ADAPTER, VMPK} from './midi-ports';
+
+function createWahrheit(): Patch {
+  const effects = [
+    new HarmonyDrum({
+      baseNoteInputFilter: filterByPort(VMPK),
+      outputPortName: MidiThroughPort,
+      trigger: filterBy(HAND_SONIC, 74),
+      noteOffsets: [12]
+    }),
+    new ControlSequenceStepper({
+      trigger: filterBy(HAND_SONIC, 74),
+      outputPortName:USB_MIDI_ADAPTER,
+      control: OSC2_SEMITONE,
+      values: [64, 95],
+      resetFilter: filterBy(HAND_SONIC, 70)
+    })
+  ];
+
+  return {
+    name: 'Wahrheit',
+    async onMidiEvent(midiEvent: MidiEvent, midiOut: MidiOut) {
+      // console.log('onMidiEvent', midiEvent, midiEvent.message);
+      for (const effect of effects) {
+        effect.onMidiEvent(midiEvent, midiOut).then(r => {/*Ignore*/
+        });
+      }
+      if (midiEvent.portName === VMPK && midiEvent.message.type === 'ControlChange') {
+        midiOut.send(MidiThroughPort, MidiMessageRaw.pitchBendChange(midiEvent.message.value))
+        const cutoffValue = mapRange([0, 127], [10, 127], midiEvent.message.value);
+        midiOut.send(MidiThroughPort, MidiMessageRaw.controlChange(CUTOFF, cutoffValue))
+      }
+    }
+  }
+}
+
+export const wahrheit = createWahrheit();
